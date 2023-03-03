1f322469ea8851ddf9a3b49351cc335d00a388ad
tawheedshimul <tawheedshimul@gmail.com>
2023-03-03 15:19:39 +0600

    üpdate

---
+++ (1)Alert/alert.js
@@ -0,0 +1,3 @@
+alert('Thank you for your input')
+
+console.log(alert);
---
+++ (2)VARIABLE/variable.js
@@ -0,0 +1,14 @@
+// variable for string 
+var thank = 'Thank you for your input';
+// alert(thank);
+console.log(thank);
+
+
+// variable for number 
+
+var hisage = 20;
+var herage =18;
+
+var totalage = hisage + herage;
+
+console.log(totalage);
