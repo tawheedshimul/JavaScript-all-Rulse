31aa64809ff35f15f942ab3d162b3601bc7a8fd4
tawheedshimul <tawheedshimul@gmail.com>
2023-03-04 14:47:11 +0600

    update

---
+++ (3)MATH EXPRESSION/math.js
@@ -0,0 +1,11 @@
+// useful number method 
+
+let lotsOfDecimal = 1.766584958675746364;
+
+
+let twoDecimalPlaces = lotsOfDecimal.toFixed(2);
+let twoDecimalPlace = Math.floor(lotsOfDecimal);
+
+
+console.log(twoDecimalPlaces);
+console.log(twoDecimalPlace);
